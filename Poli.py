92c8b10a56836408e7617a43020b00a4b530a860
Wandi1114 <59136141+Wandi1114@users.noreply.github.com>
2020-04-16 00:18:58 -0700

    Update Poli.py

--- Poli.py
+++ Poli.py
@@ -1,5 +1,4 @@
-# from Dokter import Dokter
-
+from Dokter import Dokter
 
 class Poli:
     def __init__(self):
@@ -7,22 +6,13 @@ class Poli:
         self.__daftar_dokter = []
 
     def getJenis_poli(self):
-        return "Jenis Poli : ", format(self.__jenis_poli)
+        return self.__jenis_poli
 
-    def setJenis_poli(self, jenis):
+    def addJenis_poli(self, jenis):
         self.__jenis_poli.append(jenis)
 
     def getDaftar_dokter(self):
-        return "Daftar Dokter : ", format(self.__daftar_dokter)
+        return self.__daftar_dokter
 
-    def setDaftar_dokter(self, daftar):
+    def addDaftar_dokter(self, daftar):
         self.__daftar_dokter.append(daftar)
-
-
-a = Poli()
-# d = Dokter()
-a.setDaftar_dokter(input("Masukkan Dokter :"))
-a.setJenis_poli(input("Masukkan Jenis Poli :"))
-print(a.getJenis_poli())
-print(a.getDaftar_dokter())
-# print(d.getJadwalDokter())
