5c4c498ae1f24720dd2c8f386e4ae35a4204e86e
Wandi1114 <59136141+Wandi1114@users.noreply.github.com>
2020-04-22 20:22:13 -0700

    Update and rename Dokter Lainnya.py to Dokter_Lainnya.py

--- Dokter_Lainnya.py
+++ Dokter_Lainnya.py
@@ -1,13 +1,12 @@
 from Dokter import Dokter
 
 class DokterLainnya(Dokter):
-    
         def __init__(self,ID_Dokter,Nama_Dokter,JadwalDokter,Spesialis,NoSerialDokter,Asal_Instansi):
-            super().__init__(self,ID_Dokter,Nama_Dokter,JadwalDokter,Spesialis,NoSerialDokter)
+            super().__init__(ID_Dokter,Nama_Dokter,JadwalDokter,Spesialis,NoSerialDokter)
             self.__Asal_Instansi = Asal_Instansi
 
         def getAsal_Instansi(self):
-            return self.Asal_Instansi
+            return self.__Asal_Instansi
         
-        def setAsal_INstansi(self,ai):
+        def setAsal_Instansi(self,ai):
             self.__Asal_Instansi = ai
