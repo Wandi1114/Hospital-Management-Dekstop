5b056ae933e16cb647660707b281974846ce707d
saaar-raah <61046843+saaar-raah@users.noreply.github.com>
2020-04-27 13:57:07 +0800

    Create dbase.py

---
+++ dbase.py
@@ -0,0 +1,15 @@
+from sqlalchemy import *
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import relationship, backref
+
+Base = declarative_base()
+engine = create_engine('sqlite:///Klinik.db', echo=True)
+
+Session = sessionmaker(bind=engine)
+
+
+def session():
+    Base.metadata.create_all(engine)
+    return session()
