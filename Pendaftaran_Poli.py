27af8f77d3e1d2dc7f590e9a6238b73752c02258
saaar-raah <61046843+saaar-raah@users.noreply.github.com>
2020-04-28 19:24:04 +0800

    Update Pendaftaran_Poli.py
    aku betulin biar bisa jalan fungsinya

--- Pendaftaran_Poli.py
+++ Pendaftaran_Poli.py
@@ -1,29 +1,56 @@
-#from Pasiem import Pasien
-#from Poli import Poli
+from Pasien import *
+from Poli import *
 import datetime
 dt = datetime.datetime.now()
+nama_poli = a.getNama_Poli()
+id_pasien = wandi.getID_Pasien()
 
 
-class pendaftaran_poli:
+class Pendaftaran_Poli:
+
     def __init__(self):
-        self.__waktu = ""
-        self.__tanggal = ""
-        self.__keluhan = ""
-
-    def setWaktu(self):
-        self.__waktu = dt.strftime("%I:%M %p")
+        self.__Waktu = ""
+        self.__Tanggal = ""
+        self.__Keluhan = ""
 
     def getWaktu(self):
-        return self.__waktu
+        return self.__Waktu
 
-    def setTanggal(self):
-        self.__tanggal = dt.strftime("%Y/%m/%d")
+    def setWaktu(self):
+        self.__Waktu = dt.strftime("%I:%M %p")
 
     def getTanggal(self):
-        return self.__tanggal
+        return self.__Tanggal
 
-    def setKeluhan(self, keluhan):
-        self.__keluhan = keluhan
+    def setTanggal(self):
+        self.__Tanggal = dt.strftime("%Y/%m/%d")
 
     def getKeluhan(self):
-        return self.__keluhan
+        return self.__Keluhan
+
+    def setKeluhan(self, keluhan):
+        self.__Keluhan = keluhan
+
+
+class Antri_Pasien:
+
+    def __init__(self):
+        self.__Nomor_Antrian = ""
+
+    def getNomor_Antrian(self):
+        return self.__Nomor_Antrian
+
+    def setNomor_Antrian(self, nomor):
+        self.__Nomor_Antrian = int(nomor)
+
+
+p = Antri_Pasien()
+p.setNomor_Antrian("1")
+
+x = p.getNomor_Antrian()
+y = id_pasien
+z = nama_poli
+
+print("Nomor Antrian : ", x)
+print("ID Pasien :", y)
+print("Poli : ", z)
