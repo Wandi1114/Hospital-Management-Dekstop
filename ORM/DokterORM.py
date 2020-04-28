edec92746838a7ea565b6a5fded47ad5977b9a93
Wandi1114 <59136141+Wandi1114@users.noreply.github.com>
2020-04-28 11:00:20 -0700

    Update and rename dokterORM.py to DokterORM.py

--- ORM/DokterORM.py
+++ ORM/DokterORM.py
@@ -3,17 +3,17 @@ from sqlalchemy import Column, Integer, String
 
 
 class Dokter(Base):
-    """"""
+
     __tablename__ = "Dokter"
 
     ID_Dokter = Column(Integer, primary_key=True)
-    Nama_Dokter = ""
+    Nama_Dokter = Column(String)
     Jadwal_Dokter = ""
     Spesialis = ""
     No_Serial_Dokter = ""
 
     def __init__(self, Nama_Poli, Daftar_Dokter):
-        """"""
+
         self.Nama_Poli = namaPoli
         self.Daftar_Dokter = Daftar_Dokter
 
