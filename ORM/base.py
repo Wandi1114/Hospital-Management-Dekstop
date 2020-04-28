5644966b5ab5743354a203764c7464e69cb41e68
Wandi1114 <59136141+Wandi1114@users.noreply.github.com>
2020-04-28 10:55:07 -0700

    Update base.py

--- ORM/base.py
+++ ORM/base.py
@@ -1,8 +1,10 @@
 from sqlalchemy import create_engine
-from sqlalchemy import Column, String, Integer, Text
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 Base = declarative_base()
 engine = create_engine('sqlite:///db/Kinik.db') 
 Base.metadata.create_all(engine)
+
+Session = sessionmaker(bind=engine)
+session = Session()
