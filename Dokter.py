1bde180f6dbabe2257c61ab2a59cde801471c099
saaar-raah <61046843+saaar-raah@users.noreply.github.com>
2020-04-28 19:36:42 +0800

    Update Dokter.py
    ubah dikit

--- Dokter.py
+++ Dokter.py
@@ -1,38 +1,39 @@
+
 class Dokter:
 
-    def __init__(self,ID_Dokter,Nama_Dokter,JadwalDokter,Spesialis,NoSerialDokter):
-       self.__ID_Dokter = ID_Dokter
-       self.__Nama_Dokter = Nama_Dokter
-       self.__JadwalDokter = JadwalDokter
-       self.__Spesialis = Spesialis
-       self.__NoSerialDokter = NoSerialDokter
+    def __init__(self):
+        self.__ID_Dokter = ""
+        self.__Nama_Dokter = ""
+        self.__Jadwal_Dokter = ""
+        self.__Spesialis = ""
+        self.__No_Serial_Dokter = ""
 
     def getID_Dokter(self):
         return self.__ID_Dokter
-    
-    def setID_Dokter(self,id):
+
+    def setID_Dokter(self, id):
         self.__ID_Dokter = id
 
     def getNama_Dokter(self):
         return self.__Nama_Dokter
 
-    def setNama_Dokter(self,nm):
-        self.__Nama_Dokter = nm
+    def setNama_Dokter(self, nmdok):
+        self.__Nama_Dokter = nmdok
 
-    def getJadwalDokter(self):
-        return self.__JadwalDokter
+    def getJadwal_Dokter(self):
+        return self.__Jadwal_Dokter
 
-    def setJadwal_Dokter(self,jdw):
-        self.__Jadwal_Dokter = jdw
+    def setJadwal_Dokter(self, jdwl):
+        self.__Jadwal_Dokter = jdwl
 
     def getSpesialis(self):
         return self.__Spesialis
 
-    def setSpesialis(self,spesial):
-        self.__Spesialis = spesial
+    def setSpesialis(self, spsls):
+        self.__Spesialis = spsls
 
-    def getNoSerialDokter(self):
-        return self.NoSerialDokter
+    def getNoSerial_Dokter(self):
+        return self.NoSerial_Dokter
 
-    def setNoSerialDokter(self,nsd):
-        self.__NoSerialDokter = nsd
+    def setNoSerial_Dokter(self, nsd):
+        self.__NoSerial_Dokter = nsd
