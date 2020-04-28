8899b79995f6bb1c69e334d0ecfd368bafb07640
saaar-raah <61046843+saaar-raah@users.noreply.github.com>
2020-04-28 19:21:45 +0800

    Update Poli.py
    Aku betulin poli ku

--- Poli.py
+++ Poli.py
@@ -1,18 +1,31 @@
 from Dokter import Dokter
+# from Antrian import antrian
+
+# Antrian = antrian.Antrian
+
 
 class Poli:
     def __init__(self):
-        self.__jenis_poli = []
-        self.__daftar_dokter = []
+        self.__Nama_Poli = []
+        self.__Daftar_Dokter = []
+        # self.__Antrian_Poli = Antrian
 
-    def getJenis_poli(self):
-        return self.__jenis_poli
+    def getNama_Poli(self):
+        return self.__Nama_Poli
 
-    def addJenis_poli(self, jenis):
-        self.__jenis_poli.append(jenis)
+    def setNama_Poli(self, jenis):
+        self.__Nama_Poli = jenis
 
-    def getDaftar_dokter(self):
-        return self.__daftar_dokter
+    def getDaftar_Dokter(self):
+        return self.__Daftar_Dokter
 
-    def addDaftar_dokter(self, daftar):
-        self.__daftar_dokter.append(daftar)
+    def setDaftar_Dokter(self, daftar):
+        self.__Daftar_Dokter = daftar
+
+    def getJadwal_Dokter(self, jadwal):
+        return self.__Jadwal_Dokter
+
+
+a = Poli()
+a.setNama_Poli("GIGI")
+a.getNama_Poli()
