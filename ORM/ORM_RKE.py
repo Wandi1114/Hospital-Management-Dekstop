bbb4f77681fd5b3736c390be74e43056f34dad87
Wandi1114 <59136141+Wandi1114@users.noreply.github.com>
2020-04-28 16:25:11 -0700

    Add files via upload

---
+++ ORM/ORM_RKE.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, String, Integer, Text
+from base import Session,engine,Base
+class ORMPasien(Base):
+    __tablename__ = 'RKE'
+    ID_Pasien = Column(String,nullable=False, primary_key = True)
+    namaPasien = Column(String,nullable=False)
+    Dokter = Column(String,nullable=False)
+    Diagnosa = Column(String,nullable=False)
+    
+    def __init__(self,ID,Nama,doker,Diagnosa):
+        self.ID_Pasien = ID
+        self.namaPasien = Nama
+        self.Dokter = dokter
+        self.Diagnosa = Diagnosa
