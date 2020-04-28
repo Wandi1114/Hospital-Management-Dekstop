319768fba1ac278a7ec435e07407e04f63826100
saaar-raah <61046843+saaar-raah@users.noreply.github.com>
2020-04-28 19:40:55 +0800

    Update Pasien.py
    kutambahin bawahnya aja itu, buat ngetes nyambung enggaknya sama file lain

--- Pasien.py
+++ Pasien.py
@@ -1,8 +1,12 @@
 #from Klinik import Klinik
 import datetime
+
+
 class Pasien:
     JumlahPasien = 0
-    def __init__(self,Nama):
+
+    def __init__(self, Nama):
+        self.Nama = Nama
         Pasien.JumlahPasien += 1
         self.__Nama_Lengkap = Nama
         self.__Tanggal_Lahir = "00/00/0000"
@@ -11,57 +15,61 @@ class Pasien:
         self.__Alamat = ""
         self.__No_Telp = ""
         self.__JenisKelamin = ""
-        self.__Riwayat_Penyakit = [[],[]]
+        self.__Riwayat_Penyakit = [[], []]
         self.__ID_Pasien = ""
 
         self.Daftar_Poli = []
-    
+
     def getID_Pasien(self):
         return self.__ID_Pasien
-    
+
     def setID_Pasien(self, id):
         self.__ID_Pasien = id
 
     def getNama_Lengkap(self):
         return self.__Nama_Lengkap
 
-    def setNama_Lengkap(self,nl):
+    def setNama_Lengkap(self, nl):
         self.__Nama_Lengkap = nl
-    
+
     def getTanggal_Lahir(self):
         return self.__Tanggal_Lahir
 
-    def setTanggal_Lahir(self,tl):
+    def setTanggal_Lahir(self, tl):
         self.__Tanggal_Lahir = tl
-    
+
     @property
     def Umur(self):
         self.setUmur()
         return self.__Umur
-    
+
     def setUmur(self):
         tgl = self.getTanggal_Lahir().split('/')
         self.__Umur = datetime.date.today().year - int(tgl[2])
-    
+
     def getNIK(self):
         return self.__NIK
 
-    def setNIK(self,nik):
+    def setNIK(self, nik):
         self.__NIK = nik
 
     def getNo_Telp(self):
         return self.__No_Telp
 
-    def setNo_Telp(self,notel):
+    def setNo_Telp(self, notel):
         self.__No_Telp = notel
 
     def getJenis_Kelamin(self):
         return self.__Jenis_Kelamin
 
-    def setJenis_Kelamin(self,jk):
+    def setJenis_Kelamin(self, jk):
         self.__Jenis_Kelamin = jk
-wandi=Pasien("Wandi")
-wandi.setTanggal_Lahir("02/05/2000")
-x=wandi.getTanggal_Lahir().split("/")
 
+
+wandi = Pasien("Wandi")
+wandi.setTanggal_Lahir("02/05/2000")
+x = wandi.getTanggal_Lahir().split("/")
 print(wandi.Umur)
+
+wandi.setID_Pasien("123")
+wandi.getID_Pasien()
