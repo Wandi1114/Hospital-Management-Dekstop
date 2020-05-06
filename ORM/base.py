e58f1c4a45e55832353be09986e3f1ab2faaa795
saaar-raah <61046843+saaar-raah@users.noreply.github.com>
2020-05-06 18:28:42 +0800

    Delete base.py

--- ORM/base.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-
-Base = declarative_base()
-engine = create_engine('sqlite:///db/Kinik.db') 
-Base.metadata.create_all(engine)
-
-Session = sessionmaker(bind=engine)
