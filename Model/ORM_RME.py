40cde899c7888902a2bcee6bca29d56068827cd1
saaar-raah <61046843+saaar-raah@users.noreply.github.com>
2020-05-10 17:14:21 +0800

    Update ORM_RME.py

--- Model/ORM_RME.py
+++ Model/ORM_RME.py
@@ -1,16 +1,17 @@
 from sqlalchemy import Column, String, Integer, Text, Date
-from Model.base import sessionFactory,modelFactory,Base
+from Model.base import sessionFactory, modelFactory, Base
+
 
 class ORM_RME(Base):
     __tablename__ = 'RME'
-    ID_Pasien = Column(String,nullable=False, primary_key = True)
-    namaPasien = Column(String,nullable=False)
-    Dokter = Column(String,nullable=False)
-    TanggalKonsul = Column(Date,nullable=False)
+    ID_Pasien = Column(String, nullable=False, primary_key=True)
+    namaPasien = Column(String, nullable=False)
+    Dokter = Column(String, nullable=False)
+    TanggalKonsul = Column(Date, nullable=False)
     #Diagnosa = Column(String,nullable=False)
     #Riw_Obat = Column (String,nullable=False)
-    
-    def __init__(self,ID,Nama,dokter,Diagnosa,tgl):
+
+    def __init__(self, ID, Nama, dokter, Diagnosa, tgl):
         session = sessionFactory()
         self.ID_Pasien = ID
         self.namaPasien = Nama
@@ -18,20 +19,22 @@ class ORM_RME(Base):
         #self.Diagnosa = Diagnosa
         self.TanggalKonsul = tgl
         session.add(self)
-        session.commit ()
+        session.commit()
         session.close()
 
-    def delete_RME (ID):
+    def delete_RME(ID):
         session = sessionFactory()
         session.quert(ORM_RME).filter_by(ID_Dokter=ID).delete()
         session.commit()
         session.close()
 
-    def update_RME (ID):
+    def update_RME(ID):
         pass
 
-    def view_RME ():
-        session=sessionFactory()
-        return session.query(ORM_RME),all()
+    def view_RME():
+        session = sessionFactory()
+        return session.query(ORM_RME), all()
         session.close()
+
+
 modelFactory()
