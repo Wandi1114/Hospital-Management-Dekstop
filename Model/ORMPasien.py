0d7300b0875ef1815c8217b4f089eb38a1f0efc6
saaar-raah <61046843+saaar-raah@users.noreply.github.com>
2020-05-10 17:13:22 +0800

    Update ORMPasien.py

--- Model/ORMPasien.py
+++ Model/ORMPasien.py
@@ -1,16 +1,18 @@
 from sqlalchemy import Column, String, Integer, Text
-from Model.base import sessionFactory,Base,modelFactory
+from Model.base import sessionFactory, Base, modelFactory
+
+
 class ORMPasien(Base):
     __tablename__ = 'Pasien_Klinik'
-    ID_Pasien = Column(String,nullable=False, primary_key = True)
-    namaPasien = Column(String,nullable=False)
-    Tanggal_Lahir = Column(String,nullable=False)
-    NIK = Column(String,nullable=False)
-    Alamat = Column(Text,nullable=False)
-    JenisKelamin = Column(String,nullable=False)
-    noTelpPasien = Column(String,nullable=False)
-    
-    def __init__(self,ID,Nama,TL,NIK,Almt,Notel,JK):
+    ID_Pasien = Column(String, nullable=False, primary_key=True)
+    namaPasien = Column(String, nullable=False)
+    Tanggal_Lahir = Column(String, nullable=False)
+    NIK = Column(String, nullable=False)
+    Alamat = Column(Text, nullable=False)
+    JenisKelamin = Column(String, nullable=False)
+    noTelpPasien = Column(String, nullable=False)
+
+    def __init__(self, ID, Nama, TL, NIK, Almt, Notel, JK):
         session = sessionFactory()
         self.ID_Pasien = ID
         self.namaPasien = Nama
@@ -22,16 +24,20 @@ class ORMPasien(Base):
         session.add(self)
         session.commit()
         session.close()
-    
+
     def delete_pasien(ID):
         session = sessionFactory()
         session.query(ORMPasien).filter_by(ID_Pasien=ID).delete()
         session.commit()
         session.close()
+
     def update_pasien(ID):
         pass
+
     def view_pasien():
         session = sessionFactory()
         return session.query(ORMPasien).all()
         session.close()
+
+
 modelFactory()
