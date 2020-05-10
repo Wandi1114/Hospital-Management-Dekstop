c39d09c48123dee3246d75c82193c912bf0681d1
saaar-raah <61046843+saaar-raah@users.noreply.github.com>
2020-05-10 17:13:34 +0800

    Update ORMPoli.py

--- Model/ORMPoli.py
+++ Model/ORMPoli.py
@@ -1,20 +1,35 @@
-from base import Base, Session, engine
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, String, Integer, Text
+from Model.base import sessionFactory, Base, modelFactory
 
 
-class Klinik(Base):
-    """"""
-    __tablename__ = "Poli"
-
+class ormPoli(Base):
+    __tablename__ = "Daftar_Poli"
     id = Column(Integer, primary_key=True)
-    Nama_Poli = Column(String)
-    Daftar_Dokter = Column(String)
-    # Antrian_Poli = Antrian
+    # nama_poli = Column(String)
+    # daftar_dokter = Column(String)
 
-    def __init__(self, Nama_Poli, Daftar_Dokter):
-        """"""
-        self.Nama_Poli = namaPoli
-        self.Daftar_Dokter = Daftar_Dokter
+    def __init__(self, ID, nama, daftar):
+        session = sessionFactory()
+        self.id = ID
+        # self.namaPoli = nama
+        # self.daftarDokter = daftar
+        session.add(self)
+        session.commit()
+        session.close()
+
+    def delete_poli(ID):
+        session = sessionFactory()
+        session.quert(ormPoli).filter_by(id=ID).delete()
+        session.commit()
+        session.close()
+
+    def update_poli(ID):
+        pass
+
+    def view_poli():
+        session = sessionFactory()
+        return session.query(ormPoli), all()
+        session.close()
 
 
-
+modelFactory()
