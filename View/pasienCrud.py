7ed9580be215ac99b2d6c9ac70726e9e594707a2
Wandi1114 <59136141+Wandi1114@users.noreply.github.com>
2020-05-12 23:55:09 -0700

    Update pasienCrud.py

--- View/pasienCrud.py
+++ View/pasienCrud.py
@@ -3,15 +3,15 @@
 #from Class.Pasien import Pasien
 #from Model.base import sessionFactory,modelFactory
 #from PyQt5.QtGui import QIntValidator
-from PyQt5.QtWidgets import (QApplication,QMessageBox,QMainWindow, QWidget, QPushButton,QTableWidget,QTableWidgetItem,QVBoxLayout)
+from PyQt5.QtWidgets import (QApplication,QAbstractItemView,QMessageBox,QMainWindow, QWidget,QHBoxLayout, QPushButton,QTableWidget,QTableWidgetItem,QVBoxLayout)
 import sys
+#from PyQt5.QtCore import QAbstractItemView
 from Model.ORMPasien import ORMPasien
 from View.PasienInput import InputPasien
 
 class TablePasien(QMainWindow):
     def  __init__(self):
         super(TablePasien,self).__init__()
-        #self.setGeometry(200,200,300,400)
         self.InitUI()
 
     def InitUI(self):
@@ -23,41 +23,62 @@ class TablePasien(QMainWindow):
         self.input_btn = QPushButton(self)
         self.input_btn.setText("Tambah Pasien")
         self.input_btn.adjustSize()
-        self.input_btn.clicked.connect(self.addWindow)
+        self.input_btn.clicked.connect(self.addPasien)
 
         self.close_btn = QPushButton(self)
         self.close_btn.setText("TUTUP")
         self.close_btn.adjustSize()
         self.close_btn.clicked.connect(self.closeWindow)
 
+        self.formInput = InputPasien()
+        self.formInput.setFixedSize(300,400)
+
+        self.hbox = QHBoxLayout(self)
+        self.hbox.addWidget(self.table)
+        self.hbox.addWidget(self.formInput)
+
         self.vbox = QVBoxLayout(self)
-        self.vbox.addWidget(self.table)
-        self.vbox.addWidget(self.close_btn)
+        self.vbox.addLayout(self.hbox)
         self.vbox.addWidget(self.input_btn)
+        self.vbox.addWidget(self.close_btn)
 
 
         self.container = QWidget(self)
         self.container.setLayout(self.vbox)
         self.container.adjustSize()
-    
-    def create_table(self):
-        self.table = QTableWidget(self)
-        self.table.setColumnCount(7)
-        self.table.setRowCount(len(ORMPasien.view_pasien()))
-        self.table.setHorizontalHeaderLabels(["ID","NAMA","Tanggal Lahir","NIK","Alamat","Jenis Kelamin","No. Telp."])
+        self.setCentralWidget(self.container)
 
-        self.isiTable()
-        self.table.setFixedSize(717,400)
-        
     def closeWindow(self):
         self.hide()
 
-    def addWindow(self):
-        self.form = InputPasien()
-        self.form.show()
+    def addPasien(self):
+        self.formInput.submit_btn()
+        #self.hbox.removeWidget(self.table)
+        self.isiTable()
+        #self.hbox.addWidget(self.table)
+
+    def cek(self,row):
+        print(self.table.item(row,0).text())
+        print(self.table.item(row,1).text())
+        print(self.table.item(row,2).text())
+        print(self.table.item(row,3).text())
+        print(self.table.item(row,4).text())
+        print(self.table.item(row,5).text())
+        print(self.table.item(row,6).text())
+
+
+    def create_table(self):
+        self.table = QTableWidget(self)
+        self.table.cellClicked.connect(self.cek)
+        self.table.setColumnCount(7)
+        self.table.setHorizontalHeaderLabels(["ID","NAMA","Tanggal Lahir","NIK","Alamat","Jenis Kelamin","No. Telp."])
+        self.table.setFixedSize(741,350)
+        self.table.setEditTriggers(QAbstractItemView.NoEditTriggers)
+        self.isiTable()
 
     def isiTable(self):
         query = ORMPasien.view_pasien()
+        self.table.setRowCount(len(query))
         for row in range(len(query)):
             #self.table.insertRow(1)
             self.table.setItem(row,0,QTableWidgetItem(query[row].ID_Pasien))
@@ -68,9 +89,10 @@ class TablePasien(QMainWindow):
             self.table.setItem(row,5,QTableWidgetItem(query[row].JenisKelamin))
             self.table.setItem(row,6,QTableWidgetItem(query[row].noTelpPasien))
         
-        
+         
 def crudPasien():
     app = QApplication(sys.argv)
+    #app.setStyle('fusion')
     win = TablePasien()
     win.show()
     sys.exit(app.exec_())
