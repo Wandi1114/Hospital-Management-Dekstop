14722d2926301a15b394fa11b37d51ea00fbbced
Wandi1114 <59136141+Wandi1114@users.noreply.github.com>
2020-05-12 23:55:45 -0700

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,8 +1,2 @@
-from View.pasienCrud import TablePasien
-from PyQt5.QtWidgets import QApplication
-import sys
-app = QApplication(sys.argv)
-table = TablePasien()
-table.show()
-sys.exit(app.exec_())
-#<Model.ORMPasien.ORMPasien object at 0x000000000363C3D0>
+from View.pasienCrud import crudPasien
+crudPasien()
