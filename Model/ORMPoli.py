9ceafb7dc9a753c016d5c87b448d8d1a4e1284f2
Wandi1114 <59136141+Wandi1114@users.noreply.github.com>
2020-05-13 17:36:41 -0700

    Update ORMPoli.py

--- Model/ORMPoli.py
+++ Model/ORMPoli.py
@@ -1,16 +1,16 @@
 from sqlalchemy import Column, String, Integer, Text
 from Model.base import sessionFactory, Base, modelFactory
 
-
 class ormPoli(Base):
     __tablename__ = "Daftar_Poli"
-    id = Column(Integer, primary_key=True)
+    id_poli = Column(String, primary_key=True)
     nama_poli = Column(String)
-
+    SN = 0
     def __init__(self, ID, nama):
+        ormPoli.SN+=1
         session = sessionFactory()
-        self.id = ID
-        self.namaPoli = nama
+        self.id_poli = ID+str(ormPoli.SN)
+        self.nama_poli = nama
         session.add(self)
         session.commit()
         session.close()
@@ -21,13 +21,16 @@ class ormPoli(Base):
         session.commit()
         session.close()
 
-    def update_poli(ID):
-        pass
+    def update_poli(ID,newNama):
+        session = sessionFactory()
+        session.query(ormPoli).filter_by(id_poli=ID).update({
+                ormPoli.nama_poli: newNama,
+            }, synchronize_session=False)
+        session.commit()
+        session.close()
 
     def view_poli():
         session = sessionFactory()
         return session.query(ormPoli).all()
         session.close()
-
-
 modelFactory()
